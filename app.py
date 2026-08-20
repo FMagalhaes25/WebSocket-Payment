71416224b176d99fbc15a2886472624444318c33
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-20 16:02:49 -0300

    feat: issuing a payment confirmation signal

--- app.py
+++ app.py
@@ -66,6 +66,9 @@ def pix_confirmation():
     payment.paid = True
     db.session.commit()
     
+    #Emitindo sinal para o socket io
+    socketio.emit(f'payment-confirmed-{payment.id}')
+    
     return jsonify({"message": "The payment has been confirmed!"})
 
 
@@ -73,6 +76,15 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
     
     payment = Payment.query.get(payment_id)
+    
+    if not payment:
+        return render_template('404.html')
+    
+    if payment.paid:
+        return render_template('confirmed_payment.html', 
+                               payment_id=payment_id,
+                               value=payment.value,
+                               qr_code=payment.qr_code)
     
     return render_template('payment.html', 
                            payment_id=payment_id,
@@ -85,6 +97,11 @@ def payment_pix_page(payment_id):
 @socketio.on('connect')
 def handle_connect():
     print("client connected to server")
+    
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Client has disconnected to the server")
 
 
 if __name__ == '__main__':
